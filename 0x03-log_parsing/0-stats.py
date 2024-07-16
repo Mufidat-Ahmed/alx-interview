5a9c9b52f14fec0705b23228a0848dda341f3a3e
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-07-16 17:49:28 +0100

    Edit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
-"""A script that reads stdin line by line and computes metrics"""
+'''A script that reads stdin line by line and computes metrics'''
+
 
 import sys
-import signal
 
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
 total_size = 0
-stat_code = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-count = 0
+counter = 0
 
 try:
     for line in sys.stdin:
@@ -14,15 +15,15 @@ try:
         if len(line_list) > 4:
             code = line_list[-2]
             size = int(line_list[-1])
-            if code in stat_code.keys():
-                stat_code[code] += 1
+            if code in cache.keys():
+                cache[code] += 1
             total_size += size
-            count += 1
+            counter += 1
 
-        if count == 10:
-            count = 0
+        if counter == 10:
+            counter = 0
             print('File size: {}'.format(total_size))
-            for key, value in sorted(stat_code.items()):
+            for key, value in sorted(cache.items()):
                 if value != 0:
                     print('{}: {}'.format(key, value))
 
@@ -31,6 +32,6 @@ except Exception as err:
 
 finally:
     print('File size: {}'.format(total_size))
-    for key, value in sorted(stat_code.items()):
+    for key, value in sorted(cache.items()):
         if value != 0:
             print('{}: {}'.format(key, value))
