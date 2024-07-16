12450fc97d4567e4d4ada0a900bc296c2735e06b
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-07-16 17:41:19 +0100

    edit

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -2,6 +2,7 @@
 """A script that reads stdin line by line and computes metrics"""
 
 import sys
+import signal
 
 total_size = 0
 status_codes = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
