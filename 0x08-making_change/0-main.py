4f8af8f5f3acfd82be4cea9e81ef8569ad9a63bd
Mufidat-Ahmed <imufidatahmad@gmail.com>
2024-08-22 20:26:09 +0100

    main

--- 0x08-making_change/0-main.py
+++ 0x08-making_change/0-main.py
@@ -1,4 +1,6 @@
-in file for testing
+#!/usr/bin/python3
+"""
+Main file for testing
 """
 
 makeChange = __import__('0-making_change').makeChange
@@ -6,4 +8,3 @@ makeChange = __import__('0-making_change').makeChange
 print(makeChange([1, 2, 25], 37))
 
 print(makeChange([1256, 54, 48, 16, 102], 1453))
-
